eb21f5862304d0d19c3c40160a35455a7bd3ae37
chris <2762851151@qq.com>
2025-03-16 20:42:07 +0800

    :art:update

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+from core.client import BaseClient
+from core.deepseek_client import DeepSeekClient
 from core.gemini_client import GeminiClient
+from core.gpt_client import GPTClient
 from funcs.test_build import run_test
 
 
-def main():
-    gemini = GeminiClient()
-    gemini.generate_and_save_test_cases()
+def main(model: BaseClient):
+    model.generate_and_save_test_cases()
     run_test()
-    gemini.generate_report()
+    model.generate_report()
 
 if __name__ == '__main__':
-    main()
+    gemini = GeminiClient()
+    main(gemini)
+
+    """
+    or: 
+    
+    gpt = GPTClient()
+    main(gpt)
+    
+    deepseek = DeepSeekClient()
+    main(deepseek)
+    
+    ... ...
+    
+    """
+
