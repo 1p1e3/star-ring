9dcbd95d83de97fb44b9de4e743f40eaf67501c4
chris <2762851151@qq.com>
2025-03-16 20:41:36 +0800

    :bug:fix bug

--- funcs/json_handler.py
+++ funcs/json_handler.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 from typing import Union
 
-from conf import TEST_CASE_PATH
+from conf import TEST_CASES_PATH
 
 
 def read_api_doc_from_json(file: Union[str, Path]) -> str:
@@ -20,12 +20,12 @@ def read_case_from_json(file: Union[str, Path]) -> dict:
 def write_case_to_json(data: str) -> str | None:
     try:
         final_data = json.loads(data)
-        with open(TEST_CASE_PATH, "w", encoding="utf-8") as json_file:
+        with open(TEST_CASES_PATH, "w", encoding="utf-8") as json_file:
             json.dump(final_data, json_file, ensure_ascii=False, indent=4)
 
         # 检查本地是否有该文件
-        if TEST_CASE_PATH.exists():
-            return f"用例成功写入文件: {TEST_CASE_PATH}"
+        if TEST_CASES_PATH.exists():
+            return f"用例成功写入文件: {TEST_CASES_PATH}"
 
     except Exception as e:
         print(f"用例写入文件失败: {e}")
@@ -41,6 +41,6 @@ def write_test_report_to_json(data: list):
     json_data = {
         "report": data
     }
-    with open(TEST_CASE_PATH, "w", encoding="utf-8") as json_file:
+    with open(TEST_CASES_PATH, "w", encoding="utf-8") as json_file:
         json.dump(json_data, json_file, ensure_ascii=False, indent=4)
 
